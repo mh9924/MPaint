e80e7d230d7d7d85bf6a9f7a30d8c92cfaf47274
Matt <xmatth13@live.com>
2017-09-27 18:20:36 -0400

    Major changes to design of Painting
    Removed the *2D-derived classes ColoredEllipse, ColoredLine, ColoredRectangle etc and instead used an independent "ColoredShape" class (whose instances are stored in Painting's "shapes"). This new class contains a reference to the Shape as a member variable + 2 fields/getters for draw/fill color. Significantly less code.

--- src/Painting.java
+++ src/Painting.java
@@ -1,11 +1,9 @@
-/**
- * @author Matthew Harris and Andrew Gemuenden CSC331-001 Prof. J. Tompkins
- */
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
@@ -23,15 +21,11 @@ import javax.swing.JFrame;
 public class Painting extends JComponent {
 	private static final long serialVersionUID = 1L;
 	private ArrayList<MyLine> l_lines;
-	private ArrayList<ColoredLine2D> lines;
-	private ArrayList<ColoredEllipse2D> circles;
-	private ArrayList<ColoredRectangle2D> rects;
+	private ArrayList<ColoredShape> shapes;
 	
 	public Painting(int w, int h) {
 		this.l_lines = new ArrayList<MyLine>();
-		this.lines = new ArrayList<ColoredLine2D>();
-		this.circles = new ArrayList<ColoredEllipse2D>();
-		this.rects = new ArrayList<ColoredRectangle2D>();
+		this.shapes = new ArrayList<ColoredShape>();
 		setBackground(Color.white);
 		setPreferredSize(new Dimension(w,h));
 	}
@@ -42,21 +36,13 @@ public class Painting extends JComponent {
 			g.drawLine(line.getX1(), line.getY1(), line.getX2(), line.getY2());
 		}
 		Graphics2D g2 = (Graphics2D) g;
-		for (ColoredLine2D line : this.lines) {
-			g2.setColor(new Color(line.getDrawColor()));
-			g2.draw(line);
-		}
-		for (ColoredEllipse2D circle : this.circles) {
-			g2.setColor(new Color(circle.getDrawColor()));
-			g2.draw(circle);
-			g2.setColor(new Color(circle.getFillColor()));
-			if(circle.getFillColor() != -1) { g2.fill(circle); }
-		}
-		for (ColoredRectangle2D rect : this.rects) {
-			g2.setColor(new Color(rect.getDrawColor()));
-			g2.draw(rect);
-			g2.setColor(new Color(rect.getFillColor()));
-			if(rect.getFillColor() != -1) { g2.fill(rect); }
+		for (ColoredShape shape : this.shapes) {
+			g2.setColor(new Color(shape.getDrawColor()));
+			g2.draw(shape.s);
+			if (shape.getFillColor() != -1) {
+				g2.setColor(new Color(shape.getFillColor()));
+				g2.fill(shape.s);
+			}
 		}
 	}
 	
@@ -83,30 +69,29 @@ public class Painting extends JComponent {
 					
 					int token = Integer.parseInt(vals[0].trim());
 					if (token == Tokens.LINE) {
-						ColoredLine2D line = new 
-								ColoredLine2D(Integer.parseInt(vals[1].trim()),
+						ColoredShape s = new 
+								ColoredShape(new Line2D.Double(Integer.parseInt(vals[1].trim()),
 								Integer.parseInt(vals[2].trim()),
 								Integer.parseInt(vals[3].trim()),
-								Integer.parseInt(vals[4].trim()),
-								Integer.parseInt(vals[5].trim()));														   
-						this.lines.add(line);
+								Integer.parseInt(vals[4].trim())),
+								Integer.parseInt(vals[5].trim()), -1);
+						this.shapes.add(s);
 					} else if (token == Tokens.CIRCLE) {
-						ColoredEllipse2D circle = new 
-								ColoredEllipse2D(Integer.parseInt(vals[1].trim()),
+						ColoredShape s = new 
+								ColoredShape(new Ellipse2D.Double(Integer.parseInt(vals[1].trim()),
 								Integer.parseInt(vals[2].trim()),
 								Integer.parseInt(vals[3].trim()),
-								Integer.parseInt(vals[4].trim()),
-								Integer.parseInt(vals[5].trim()));
-						this.circles.add(circle);
+								Integer.parseInt(vals[3].trim())),
+								Integer.parseInt(vals[4].trim()), Integer.parseInt(vals[5].trim()));
+						this.shapes.add(s);
 					} else if (token == Tokens.RECT) {
-						ColoredRectangle2D rect = new 
-								ColoredRectangle2D(Integer.parseInt(vals[1].trim()),
+						ColoredShape s = new 
+								ColoredShape(new Rectangle2D.Double(Integer.parseInt(vals[1].trim()),
 								Integer.parseInt(vals[2].trim()),
 								Integer.parseInt(vals[3].trim()),
-								Integer.parseInt(vals[4].trim()),
-								Integer.parseInt(vals[5].trim()),
-								Integer.parseInt(vals[6].trim()));
-						this.rects.add(rect);
+								Integer.parseInt(vals[4].trim())),
+								Integer.parseInt(vals[5].trim()), Integer.parseInt(vals[6].trim()));
+						this.shapes.add(s);
 					}
 					
 				}
@@ -147,33 +132,14 @@ public class Painting extends JComponent {
 		public static final int QUADCURVE = 12;
 	}
 	
-	public class ColoredLine2D extends Line2D.Double {
+	public class ColoredShape {
 		private static final long serialVersionUID = 1L;
-		private int drawcolor;
-		
-		public ColoredLine2D(int x1, int x2, int y1, int y2, int drawcolor) {
-			super(x1,x2,y1,y2);
-			this.drawcolor = drawcolor;
-		}
-		
-		public int getDrawColor() { return this.drawcolor; }
-		
-	}
-	
-	public class ColoredEllipse2D extends Ellipse2D.Double {
-		private static final long serialVersionUID = 1L;
+		public Shape s;
 		private int drawcolor;
 		private int fillcolor;
 		
-		public ColoredEllipse2D(int x, int y, int width, int height, int drawcolor, int fillcolor) {
-			super(x,y,width,height);
-			this.drawcolor = drawcolor;
-			this.fillcolor = fillcolor;
-		}
-		
-		// Constructor for circle
-		public ColoredEllipse2D(int x, int y, int diameter, int drawcolor, int fillcolor) {
-			super(x,y,diameter,diameter);
+		public ColoredShape(Shape shape, int drawcolor, int fillcolor) {
+			this.s = shape;
 			this.drawcolor = drawcolor;
 			this.fillcolor = fillcolor;
 		}
@@ -181,25 +147,9 @@ public class Painting extends JComponent {
 		public int getDrawColor() { return this.drawcolor; }
 		
 		public int getFillColor() { return this.fillcolor; }
-
+		
 	}
 	
-	public class ColoredRectangle2D extends Rectangle2D.Double {
-		private static final long serialVersionUID = 1L;
-		private int drawcolor;
-		private int fillcolor;
-		
-		public ColoredRectangle2D(int x, int y, int width, int height, int drawcolor, int fillcolor) {
-			super(x,y,width,height);
-			this.drawcolor = drawcolor;
-			this.fillcolor = fillcolor;
-		}
-		
-		public int getDrawColor() { return this.drawcolor; }
-		
-		public int getFillColor() { return this.fillcolor; }
-
-	}
 	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame();
